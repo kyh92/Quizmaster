0a619e83313dbda1cea55e35a3e7a5c588a437d8
Kit Yee Hoang Wu <kit.yee.hoang.wu@brightstraining.com>
2024-11-05 11:48:05 +0100

    Created test skeleton

--- QuizmasterGame.Tests/UnitTest1.cs
+++ QuizmasterGame.Tests/UnitTest1.cs
@@ -2,9 +2,59 @@ namespace QuizmasterGame.Tests;
 
 public class UnitTest1
 {
+   
     [Fact]
-    public void Test1()
+    public void RandomQuestions()
     {
 
     }
+
+    [Fact]
+    public void CorrectAnswer()
+    {
+
+    }
+
+    [Fact]
+    public void IncorrectAnswer()
+    {
+
+    }
+
+    [Fact]
+    public void GetPoints()
+    {
+
+    }
+
+    [Fact]
+    public void MinusPoints()
+    {
+
+    }
+
+    [Fact]
+    public void ReadFromFile()
+    {
+
+    }
+
+    [Fact]
+    public void FailMissingFile()
+    {
+
+    }
+
+    [Fact]
+    public void TimeTimer()
+    {
+
+    }
+
+    [Fact]
+    public void TimeOut()
+    {
+
+    }    
+    
 }
