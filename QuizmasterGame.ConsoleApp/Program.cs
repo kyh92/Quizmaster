c5bacf2fa37bb115aaa8c646c063c1e24b35dc34
Kit Yee Hoang Wu <kit.yee.hoang.wu@brightstraining.com>
2024-11-06 18:28:56 +0100

    Changed textcolor, added gameover text, no points added when time is over and separated  correct/incorrect answer when time is over.

--- QuizmasterGame.ConsoleApp/Program.cs
+++ QuizmasterGame.ConsoleApp/Program.cs
@@ -13,7 +13,9 @@ internal class Program
         Console.Clear();
         Console.SetBufferSize(Console.BufferWidth, 200);
 
-        System.Console.WriteLine($"Welcome to Quizmaster!");
+        Console.ForegroundColor = ConsoleColor.DarkCyan;
+        Console.WriteLine($"Welcome to Quizmaster!✍️  🥸");
+        Console.WriteLine();
 
         try
         {
@@ -28,12 +30,15 @@ internal class Program
             {
                 quizmasterGame.StartTimer(10);
                 // Display question text and options
+                Console.ForegroundColor = ConsoleColor.DarkCyan;
                 Console.WriteLine($"Question: {question.QuestionText}");
+                Console.ForegroundColor = ConsoleColor.DarkYellow;
+                
                 for (int i = 0; i < question.Options.Count; i++)
                 {
                     Console.WriteLine($"Option {i + 1}: {question.Options[i]}");
                 }
-
+                Console.ForegroundColor = ConsoleColor.White;
                 // Save the current cursor position to keep countdown on a single line
                 int countdownLine = Console.CursorTop;
 
@@ -54,7 +59,9 @@ internal class Program
                         if (quizmasterGame.TimeLeft < 0)
                         {
                             Console.SetCursorPosition(0, countdownLine);
+                            Console.ForegroundColor = ConsoleColor.DarkRed;
                             Console.WriteLine("Time's up!                                     ");
+                            Console.ForegroundColor = ConsoleColor.White;
                             Console.SetCursorPosition(18, countdownLine + 1);
                         }
                     }, timerCts.Token);
@@ -68,15 +75,31 @@ internal class Program
                 if (quizmasterGame.CorrectAnswer(question.CorrectOption, selectedOption) && quizmasterGame.TimeLeft > 0)
                 {
                     quizmasterGame.AddPoint();
+                    Console.ForegroundColor = ConsoleColor.DarkGreen;
                     Console.WriteLine($"Correct answer is option {question.CorrectOption}: {question.Options[question.CorrectOption - 1]}");
+                    Console.ForegroundColor = ConsoleColor.DarkBlue;
                     Console.WriteLine($"You currently have {quizmasterGame.points} points!");
+                    Console.ForegroundColor = ConsoleColor.White;
 
+                }
+                else if (quizmasterGame.CorrectAnswer(question.CorrectOption, selectedOption) && quizmasterGame.TimeLeft <= 0)
+                {
+                    Console.ForegroundColor = ConsoleColor.DarkGray;
+                    Console.WriteLine($"Correct answer is option {question.CorrectOption}: {question.Options[question.CorrectOption - 1]}");
+                    Console.ForegroundColor = ConsoleColor.DarkBlue;
+                    Console.WriteLine($"You currently have {quizmasterGame.points} points!");
+                    Console.ForegroundColor = ConsoleColor.White;
                 }
                 else
                 {
+                   
+                    Console.ForegroundColor = ConsoleColor.DarkRed;
                     Console.WriteLine($"Correct answer is option {question.CorrectOption}: {question.Options[question.CorrectOption - 1]}");
+                    Console.ForegroundColor = ConsoleColor.DarkBlue;
                     Console.WriteLine($"You currently have {quizmasterGame.points} Points!");
+                    Console.ForegroundColor = ConsoleColor.White;
                 }
+
 
 
                 quizmasterGame.StopTimer();
@@ -84,6 +107,12 @@ internal class Program
 
                 Console.WriteLine(); // Add a blank line for readability
             }
+            Console.ForegroundColor = ConsoleColor.DarkRed;
+            Console.WriteLine("Game Over!");
+            Console.ForegroundColor = ConsoleColor.DarkBlue;
+            Console.WriteLine($"You got {quizmasterGame.points} points.");
+            Console.ForegroundColor = ConsoleColor.White;
+            Console.WriteLine();
         }
         catch (Exception ex)
         {
